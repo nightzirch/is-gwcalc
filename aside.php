a4e2dd512cd0ede2d405a70b745ea924e8f927f8
nightzirch <chisse1049@hotmail.com>
2013-06-23 19:11:32 -0400

    Some structural changes, added aside.php

---
+++ aside.php
@@ -0,0 +1,110 @@
+<h2>Attributes</h2>
+<table>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-power"></span>
+			</div>
+		</td>
+		<td id="power" class="value">916</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-precision"></span>
+			</div>
+		</td>
+		<td id="precision" class="value">916</td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-toughness"></span>
+			</div>
+		</td>
+		<td id="toughness" class="value">916</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-vitality"></span>
+			</div>
+		</td>
+		<td id="vitality" class="value">916</td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-criticaldamage"></span>
+			</div>
+		</td>
+		<td id="criticaldamage" class="value">0%</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-conditiondamage"></span>
+			</div>
+		</td>
+		<td id="conditiondamage" class="value">0</td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-conditionduration"></span>
+			</div>
+		</td>
+		<td id="conditionduration" class="value">0%</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-healingpower"></span>
+			</div>
+		</td>
+		<td id="healingpower" class="value">0</td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-boonduration"></span>
+			</div>
+		</td>
+		<td id="boonduration" class="value">0%</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-critical"></span>
+			</div>
+		</td>
+		<td id="criticalchance" class="value"><?php echo round((916 - 822) / 21).'%'; ?></td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-damage"></span>
+			</div>
+		</td>
+		<td id="damage" class="value">0%</td>
+	</tr>
+	<tr class="even">
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-armor"></span>
+			</div>
+		</td>
+		<td id="armor" class="value"><?php echo $profession->armor; ?></td>
+	</tr>
+	<tr>
+		<td class="att">
+			<div class="traits_iconcontainer">
+				<span class="traits-health"></span>
+			</div>
+		</td>
+		<td id="health" class="value"><?php echo $profession->health+916*10; ?></td>
+	</tr>
+
+</table>
+
+<div id="rune_bonus"></div>
